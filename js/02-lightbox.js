7f4fbbcac81ee5d6a808bf28bfed2f2c9dd64e02
Anna Rakarchuk <anytarakarchuk@gmail.com>
2022-08-28 21:15:57 +0300

    Add decision of 02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+//Створення розмітки
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+new SimpleLightbox('.gallery a', {
+	animationSpeed: 150,
+	captionsData: 'alt',
+	captionDelay: 250,
+});
 
 console.log(galleryItems);
